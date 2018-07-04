b6368bd29ec9bc07c4b1e46a296c528a01eca01e
root <497028487@qq.com>
2018-07-04 13:21:51 +0800

    stable with serial and network

---
+++ serialradiodatadeal.cpp
@@ -0,0 +1,5 @@
+#include "serialradiodatadeal.h"
+
+SerialRadioDataDeal::SerialRadioDataDeal()
+{
+}
---
+++ serialradiodatadeal.h
@@ -0,0 +1,12 @@
+#ifndef SERIALRADIODATADEAL_H
+#define SERIALRADIODATADEAL_H
+
+#include <QThread>
+
+class SerialRadioDataDeal : public QThread
+{
+public:
+    SerialRadioDataDeal();
+};
+
+#endif // SERIALRADIODATADEAL_H
